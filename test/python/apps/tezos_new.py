70691a9a50bd03567828970f5e5d2bf75a88cf91
Emma Turner <emma.turner@trili.tech>
2024-01-05 14:34:51 +0000

    swap: remove coin configuration for tezos
    configuration not supplied, as tokens on tezos are not supported in
    swap, only tez

--- test/python/apps/tezos_new.py
+++ test/python/apps/tezos_new.py
@@ -14,7 +14,7 @@ from ragger.bip import pack_derivation_path
 from ragger.utils import create_currency_config, RAPDU
 
 
-XTZ_CONF = create_currency_config("XTZ", "Tezos Wallet", ("XTZ", 6))
+XTZ_CONF = create_currency_config("XTZ", "Tezos Wallet")
 
 XTZ_PACKED_DERIVATION_PATH = pack_derivation_path("m/44'/1729'/0'")
 
